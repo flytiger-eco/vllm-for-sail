be95ad56cd10c70f691e5e94ede50e97c348abae
kasakinozomi123 <zhouzhiyi923@163.com>
2026-03-19 11:38:23 +0800

    [QUANT][MARLIN]: Marlin opt

--- csrc/moe/marlin_moe_wna16/ops.cu
+++ csrc/moe/marlin_moe_wna16/ops.cu
@@ -25,6 +25,7 @@
 
 #include "kernel.h"
 #include "core/registration.h"
+#include <cstdlib>
 
 #define STATIC_ASSERT_SCALAR_TYPE_VALID(scalar_t)               \
   static_assert(std::is_same<scalar_t, half>::value ||          \
@@ -309,6 +310,9 @@ exec_config_t determine_exec_config(
 
     if (kernel == MarlinDefault) continue;
 
+    if (std::getenv("PPU_SDK")) {
+      return {1, th_config};
+    }
     cudaFuncAttributes attr;
     cudaFuncGetAttributes(&attr, kernel);
     int reg_size = max(attr.numRegs, 1) * th_config.num_threads * 4;
@@ -498,6 +502,23 @@ void marlin_mm(const void* A, const void* B, void* C, void* C_tmp, void* b_bias,
               ", has_zp = ", has_zp, ", is_zp_float = ", is_zp_float,
               ", max_shared_mem = ", max_shared_mem);
 
+  if (std::getenv("SAIL_MARLIN_MOE_PRINT_CONFIG"))
+    std::cout << "thread config:"
+              << " m_block_size_8 = " << m_block_size_8
+              << ", thread_m_blocks = " << thread_m_blocks
+              << ", thread_k = " << thread_tfg.thread_k
+              << ", thread_n = " << thread_tfg.thread_n
+              << ", num_threads = " << thread_tfg.num_threads
+              << ", blocks_per_sm = " << exec_cfg.blocks_per_sm
+              << " for MKN = [" << prob_m << ", " << prob_k << ", " << prob_n
+              << "] and num_bits = " << num_bits
+              << ", group_size = " << group_size
+              << ", has_act_order = " << has_act_order
+              << ", is_k_full = " << is_k_full << ", has_zp = " << has_zp
+              << ", is_zp_float = " << is_zp_float
+              << ", max_shared_mem = " << max_shared_mem << ", sms = " << sms
+              << std::endl;
+
   int sh_cache_size =
       get_kernel_cache_size(thread_tfg, m_block_size_8, thread_m_blocks, prob_m,
                             prob_n, prob_k, num_bits, group_size, has_act_order,
@@ -669,7 +690,15 @@ torch::Tensor moe_wna16_marlin_gemm(
 
   // sms: number of SMs to use for the kernel
   int sms = -1;
-  cudaDeviceGetAttribute(&sms, cudaDevAttrMultiProcessorCount, a.get_device());
+  cudaDeviceProp deviceProp;
+  cudaGetDeviceProperties(&deviceProp, a.get_device());
+  std::string deviceName(deviceProp.name);
+  if (deviceName.find("ZW810E") != std::string::npos) {
+    sms = 20;
+  } else {
+    cudaDeviceGetAttribute(&sms, cudaDevAttrMultiProcessorCount,
+                           a.get_device());
+  }
 
   // Alloc buffers
   const at::cuda::OptionalCUDAGuard device_guard(device_of(a));
