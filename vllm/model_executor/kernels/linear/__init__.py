39fdd25409536e028f7646926fa88c5eca3a8cb1
kasakinozomi123 <zhouzhiyi923@163.com>
2026-03-31 14:56:32 +0800

    [LINEAR][DG]: Support DeepGemm INT8/FP8 W8A8 dense + channel-wise FP8

--- vllm/model_executor/kernels/linear/__init__.py
+++ vllm/model_executor/kernels/linear/__init__.py
@@ -179,7 +179,10 @@ from vllm.model_executor.kernels.linear.scaled_mm.rocm import (
     ROCmFP8ScaledMMLinearKernel,
 )
 from vllm.model_executor.kernels.linear.scaled_mm.ppu import (
-    PPUFP8ScaledMMLinearKernel,
+    PPUDeepGemmFP8ScaledMMLinearKernel,
+    PPUDeepGemmFp8BlockScaledMMKernel,
+    PPUCutlassFp8BlockScaledMMKernel,
+    PPUCutlassFP8ScaledMMLinearKernel,
     PPUInt8ScaledMMLinearKernel,
 )
 from vllm.model_executor.kernels.linear.scaled_mm.triton import (
@@ -337,7 +340,8 @@ _POSSIBLE_FP8_KERNELS: dict[PlatformEnum, list[type[FP8ScaledMMLinearKernel]]] =
         HummingFP8ScaledMMLinearKernel,
     ],
     PlatformEnum.PPU: [
-        PPUFP8ScaledMMLinearKernel,
+        PPUDeepGemmFP8ScaledMMLinearKernel,
+        PPUCutlassFP8ScaledMMLinearKernel,
         PerTensorTorchFP8ScaledMMLinearKernel,
         ChannelWiseTorchFP8ScaledMMLinearKernel,
     ],
@@ -374,6 +378,8 @@ _POSSIBLE_FP8_BLOCK_KERNELS: dict[
         HummingFP8ScaledMMLinearKernel,
     ],
     PlatformEnum.PPU: [
+        PPUDeepGemmFp8BlockScaledMMKernel,
+        PPUCutlassFp8BlockScaledMMKernel,
         MarlinFP8ScaledMMLinearKernel,
         TritonFp8BlockScaledMMKernel,
     ],
@@ -1081,7 +1087,10 @@ __all__ = [
     "CPUInt8ScaledMMLinearKernel",
     "CutlassFP8ScaledMMLinearKernel",
     "CutlassInt8ScaledMMLinearKernel",
-    "PPUFP8ScaledMMLinearKernel",
+    "PPUDeepGemmFP8ScaledMMLinearKernel",
+    "PPUDeepGemmFp8BlockScaledMMKernel",
+    "PPUCutlassFp8BlockScaledMMKernel",
+    "PPUCutlassFP8ScaledMMLinearKernel",
     "PPUInt8ScaledMMLinearKernel",
     "FlashInferFP8ScaledMMLinearKernel",
     "ChannelWiseTorchFP8ScaledMMLinearKernel",
