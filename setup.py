c053192ca92d338180bbe57ab7c8f927329b3b26
kasakinozomi123 <zhouzhiyi923@163.com>
2026-05-12 15:30:48 +0800

    [PPU]: freeze version and requirements
    - Pin SETUPTOOLS_SCM_PRETEND_VERSION to 0.24.0
    - Comment out some dependency as workaround for build install failure

--- setup.py
+++ setup.py
@@ -1057,6 +1057,9 @@ def get_nvcc_cuda_version() -> Version:
 
 
 def get_vllm_version() -> str:
+    # PPU FIXME: Explicitly set vLLM version for PPU pypi
+    os.environ["SETUPTOOLS_SCM_PRETEND_VERSION"] = "0.24.0"
+
     # Allow overriding the version. This is useful to build platform-specific
     # wheels (e.g. CPU, TPU) without modifying the source.
     if env_version := os.getenv("VLLM_VERSION_OVERRIDE"):
